aa16a5f55204ec17360ec0863e4b2def2a577c58
thang294 <thang290495@gmail.com>
2021-04-25 17:12:48 +0700

    changed path

--- src/app.ts
+++ src/app.ts
@@ -29,9 +29,9 @@ app.use((req, res, next) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static(path.join(__dirname, '../', 'client', 'build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../', 'client', 'build', 'index.html'));
 });
 
 // load mongodb - START
